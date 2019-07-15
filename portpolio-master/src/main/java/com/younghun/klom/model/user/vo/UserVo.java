fb80d7cd11e537769bfb791590315caf3a520f41
Administrator <Administrator@yonghun-PC>
2019-07-15 13:16:00 +0900

    db변경으로 인한 수정

--- portpolio-master/src/main/java/com/younghun/klom/model/user/vo/UserVo.java
+++ portpolio-master/src/main/java/com/younghun/klom/model/user/vo/UserVo.java
@@ -9,9 +9,10 @@ import lombok.Setter;
 @Getter
 // UserInfo
 public class UserVo {
+	private int id;
 	private String image;			//UserImage
 	private String email;   		//UserEmail
-	private String name;			//UserName	(P)
+	private String name;			//UserName	
 	private String password;		//UserPassword
 	private String introduce;		//UserIntroduce
 	private Date date;				//UserDate
