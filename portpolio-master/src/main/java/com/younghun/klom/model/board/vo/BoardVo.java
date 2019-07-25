c3d1cee02de6c0704583901a62344ca5ab273042
Administrator <Administrator@yonghun-PC>
2019-07-25 23:55:42 +0900

    Long타입 -> int타입으로 변경

--- portpolio-master/src/main/java/com/younghun/klom/model/board/vo/BoardVo.java
+++ portpolio-master/src/main/java/com/younghun/klom/model/board/vo/BoardVo.java
@@ -11,10 +11,11 @@ public class BoardVo {
 
 	// BoardInfo
 	private Long no;			    //boardId	(P)
-	private Long userId;		    //UserId	(F)
+	private int userId;		    //UserId	(F)
 	private String title;			//boardTitle
 	private String writer;			//boardWriter	
 	private String content;			//boardContent
 	private String date;			//boardDate
+	private int recent;				// **게시글 댓글 수 추가 
 	  
 }
